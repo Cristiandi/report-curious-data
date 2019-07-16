81a2ebe739e227a8c6cc331009db2824a9117bfd
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2019-07-15 19:31:04 -0500

    feat(api): adding seed

---
+++ packages/api/seeds/01_tags.js
@@ -0,0 +1,29 @@
+
+exports.seed = async (knex) => {
+  const tags = [
+    {
+      id: 1,
+      name: 'Ciencia'
+    },
+    {
+      id: 2,
+      name: 'Tecnología'
+    },
+    {
+      id: 3,
+      name: 'Cosmología'
+    }
+  ];
+
+  const promises = tags.map(async tag => {
+    const rows = await knex('tags').select('id').where('name', tag.name);
+    if (rows.length === 0) {
+      const id = (await knex('tags').insert(tag))[0];      
+      return { id };
+    }
+    return { ...rows[0] };
+  });
+
+  const rows = await Promise.all(promises);
+  console.log(rows);
+};
